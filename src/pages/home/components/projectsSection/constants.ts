1094d2b9f2d2903fdbf6ec6aa944de583af379b5
Matheus Gonçalves <matheus.goncalves@hotmart.com>
2024-02-04 13:35:55 -0300

    chore: adding react router to the project tech stack

--- src/pages/home/components/projectsSection/constants.ts
+++ src/pages/home/components/projectsSection/constants.ts
@@ -8,7 +8,7 @@ export const PROJECT_LIST: ProjectList = [
     title: 'Top Rated Movies App',
     description:
       'Immerse yourself in the captivating world of movies with Movies.app showcase, a project that not only showcases my skills as a frontend developer but also provides an enjoyable and visually stunning way to discover and explore top-rated films. This project is a dynamic web application that seamlessly connects with The Movie Database (TMDb) API to bring you the latest and top-rated movies right to your fingertips. Immerse yourself in the world of cinema with a sleek and interactive grid format, complete with captivating animations triggered when you hover over each movie card.',
-    stack: 'TypeScript, React, React Query, Vite, Tailwind',
+    stack: 'TypeScript, React, React router, React Query, Vite, Tailwind',
     github: 'https://github.com/matheuscorrea70/app-movies',
     liveDemo: 'https://app-movies-eight.vercel.app'
   },
